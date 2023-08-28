5ea9987a07025ccb2254d9fc8f7ea60de3f2fba3
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-28 17:02:55 +0300

    task-01 complete without ESC

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onClick);
+
+const createImageListItem = galleryItems.map(
+  (image) =>
+    `<li class="gallery__item"> <a class="gallery__link" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"></a></li>`
+);
+
+gallery.insertAdjacentHTML("afterbegin", createImageListItem.join(""));
+
+const getAllLinksImage = document.querySelectorAll(".gallery__link");
+getAllLinksImage.forEach((link) =>
+  link.addEventListener("click", imageLinkToDefault)
+);
+
+function imageLinkToDefault(event) {
+  event.preventDefault();
+}
+
+function onClick(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+  if (isEscKey) {
+    instance.show();
+  }
+}
