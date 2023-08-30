c93a6b517e8111f07782660fb21d5a38ef45a898
cloudwipe <roma.isaev.2000@gmail.com>
2023-08-30 19:26:57 +0300

    task-02 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createImageListItem = galleryItems.map(
+  (image) =>
+    `<li class="gallery__item"> 
+    <a class="gallery__link" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}">
+    </a>
+    </li>`
+);
+
+gallery.insertAdjacentHTML("afterbegin", createImageListItem.join(""));
+
+const getAllLinksImage = document.querySelectorAll(".gallery__link");
+getAllLinksImage.forEach((link) =>
+  link.addEventListener("click", imageLinkToDefault)
+);
+
+function imageLinkToDefault(event) {
+  event.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
