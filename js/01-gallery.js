522768202b775b2f650c6424d1ebb0bc5c3830a6
cloudwipe <roma.isaev.2000@gmail.com>
2023-09-02 11:27:01 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,20 +27,35 @@ function onShow(event) {
   }
 
   const instance = basicLightbox.create(
-    `<img src="${event.target.dataset.source}" width="800" height="600">`
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", closeModuleEsc);
+        function closeModuleEsc(event) {
+          const ESC_KEY_CODE = "Escape";
+          const isEscKey = event.code === ESC_KEY_CODE;
+          if (isEscKey) {
+            window.removeEventListener("keydown", closeModuleEsc);
+            return instance.close();
+          }
+        }
+      },
+    }
   );
 
   instance.show();
 
-  if (instance.show()) {
-    window.addEventListener("keydown", closeModuleEsc);
-  }
+  // if (instance.show()) {
+  //   window.addEventListener("keydown", closeModuleEsc);
+  // }
 
-  function closeModuleEsc(event) {
-    const ESC_KEY_CODE = "Escape";
-    const isEscKey = event.code === ESC_KEY_CODE;
-    if (isEscKey) {
-      return instance.close();
-    }
-  }
+  // function closeModuleEsc(event) {
+  //   const ESC_KEY_CODE = "Escape";
+  //   const isEscKey = event.code === ESC_KEY_CODE;
+  //   if (isEscKey) {
+  //     return instance.close();
+  //   }
+  // }
 }
+
+// window.removeEventListener("keydown", closeModuleEsc);
